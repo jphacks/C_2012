bbf62d57bf7ed0b4d4941cd6ae814c4fdcc2cff6
H0R15H0 <shohori13@gmail.com>
2020-11-02 13:13:36 +0900

    カメラを起動する #3 canvasに出力した。

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
   return render_template("index.html", message="Hello nikoha!")
 
+@app.route('/video/')
+def video():
+	return render_template("video.html")
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
