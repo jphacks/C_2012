dc5a1ab699f4f8689a2e90312d9acb53a9065535
H0R15H0 <shohori13@gmail.com>
2020-11-05 22:39:47 +0900

    印鑑画像確認ページの作成 #20 app.pyに諸々コメント付与。

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import cv2
 import create_stamps.create_stamps as create_stamps
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template("index.html", message="Hello nikoha!")
+	if request.method == 'POST':
+		# sessionに必要事項を保存
 
-@app.route('/video')
-def video():
-	return render_template("video.html")
+		# pdf生成
+
+		# 印鑑生成もここ？
+		# content = request.form['content'] # ex. content = '{"husband": {"last_name": "佐藤"},"wife": {"last_name": "田中"}}'
+		# husband_last_name, wife_last_name = create_stamps.load_files(content)
+		# husband_last_name_img = create_stamps.create_last_name(husband_last_name, 'husband')
+		# husband_last_name_img = create_stamps.create_last_name(wife_last_name, 'wife')
+		# create_stamps.combine_images('husband')
+		# create_stamps.combine_images('wife')
+		return redirect(url_for("confirm_stamps"))
+	else:
+		return render_template("index.html", message="Hello nikoha!")
 
 @app.route('/confirm_stamps')
 def confirm_stamps():
-  husband_last_name, wife_last_name = create_stamps.load_files('{"husband": {"last_name": "佐藤"},"wife": {"last_name": "田中"}}')
-  husband_last_name_img = create_stamps.create_last_name(husband_last_name, 'husband')
-  husband_last_name_img = create_stamps.create_last_name(wife_last_name, 'wife')
-  create_stamps.combine_images('husband')
-  create_stamps.combine_images('wife')
-  return render_template("confirm_stamps.html", image="image")
+	# ディレクトリ直置きはまずいのでココでSession取り出して印鑑画像生成する（Want to do）
+	return render_template("confirm_stamps.html")
+
+@app.route('/video', methods=['GET', 'POST'])
+def video():
+	if request.method == 'POST':
+		# pdf（必要事項、印鑑、写真） 作成処理？
+		return # redirect(url_for("NEXT_PAGE"))
+	else:
+		return render_template("video.html")
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+	app.run(host='0.0.0.0', port=5000, debug=True)
