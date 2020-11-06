055f2efbdd35898b78e86648b54c52dd19077e3b
yunine16 <yuchin616@icloud.com>
2020-11-06 17:32:25 +0900

    入力事項の入力受付 #9 沼ってきた

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template("index.html", message="Hello nikoha!")
+    return render_template("index.html", message="Hello nikoha!")
+
+@app.route('/input')
+def input():
+    return render_template("input.html")
 
 @app.route('/video')
 def video():
-	return render_template("video.html")
+    return render_template("video.html")
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
